2fcd121f697c9f57e944852a69c74823f529f71b
yotam frank <yotamfr@yahoo.com>
2015-04-20 18:22:22 +0300

    Finished adding Definitions, Declarations. Started adding constraints.

--- asv15_ex1/planning.py
+++ asv15_ex1/planning.py
@@ -84,7 +84,7 @@ def print_plan(city_packages, city_airplanes, airplane_packages):
     print
 
 
-def get_transport_plan(nc, np, na, src, dst, start):
+def get_transport_plan(nc, np, na, src, dst, start, t_max=3):
     # Declaring sorts
     airplane = DeclareSort('A')
     package = DeclareSort('P')
@@ -112,10 +112,9 @@ def get_transport_plan(nc, np, na, src, dst, start):
 
     # Defining Predicates and Functions
     s = Solver()
-    s.set(soft_timeout=5000)
+    s.set(soft_timeout=30000)
 
-    t_max = 3
-    s.add(And(t >= 0, t <= t_max))
+    # s.add(ForAll([t], And(t >= 0, t <= t_max)))
 
     # All packages start at their source cities
     for idx in range(np):
@@ -129,74 +128,56 @@ def get_transport_plan(nc, np, na, src, dst, start):
     for idx in range(na):
         s.add(loc(aps[idx], 0) == cts[start[idx]])
 
-    # move(airplane a, time t) := loc(a, t) != loc(a, t+1)
-    s.add(ForAll([a], ForAll([t],
-          And(
-              Implies(move(a, t), loc(a, t) == loc(a, t + 1)),
-              Implies(loc(a, t) == loc(a, t + 1), move(a, t))))))
+    # At t==0, no package is loaded on no airplane!
+    s.add(ForAll([p], ForAll([a], Not(on(p, a, 0)))))
 
-    # At any time for any package there exists one and only airplane/city where it is located on/at.
-    s.add(ForAll([t], ForAll([p],
-                             Or(
+    for idx in range(na):
+        for jdx in range(idx+1, na):
+            s.add(aps[idx] != aps[jdx])
+
+    for idx in range(nc):
+        for jdx in range(idx+1, nc):
+            s.add(cts[idx] != cts[jdx])
+
+    for idx in range(np):
+        for jdx in range(idx+1, np):
+            s.add(pks[idx] != pks[jdx])
+
+    # move(airplane a, time t) := loc(a, t) != loc(a, t+1)
+    s.add(ForAll([a], ForAll([t], move(a, t) == (loc(a, t) != loc(a, t + 1)))))
+
+    # For any package at any time there exists one and only airplane/city where it is located on/at.
+    s.add(ForAll([p], ForAll([t],
+                             Xor(
                                  And(
                                      ForAll([a], Not(on(p, a, t))),
                                      Exists([c_tag], And(
                                          at(p, c_tag, t),
-                                         ForAll([c], Implies(at(p, c, t), c == c_tag))))),
+                                         ForAll([c], Implies(
+                                             at(p, c, t),
+                                             c == c_tag))))),
                                  And(
                                      ForAll([c], Not(at(p, c, t))),
                                      Exists([a_tag], And(
                                          on(p, a_tag, t),
-                                         ForAll([a], Implies(on(p, a, t), a == a_tag)))))))))
+                                         ForAll([a], Implies(
+                                             on(p, a, t),
+                                             a == a_tag)))))))))
 
     # Any plane at any time must be present at one and only city
     s.add(ForAll([a], ForAll([t], Exists([c_tag],
-                                         And(
-                                             loc(a, t) == c_tag,
-                                             ForAll([c], Implies(loc(a, t) == c, c == c_tag)))))))
+                                         And(loc(a, t) == c_tag, ForAll([c], Implies(loc(a, t) == c, c == c_tag)))))))
 
     # Rule of load: Airplane must be at the same city as the package and not move. Package must load to airplane at t+1.
     s.add(ForAll([p], ForAll([a], ForAll([t],
-                                         And(
-                                             Implies(
-                                                 load(p, a, t),
-                                                 And(Not(move(a, t)), at(p, loc(a, t), t), on(p, a, t+1))),
-                                             Implies(
-                                                 And(Not(move(a, t)), at(p, loc(a, t), t), on(p, a, t+1)),
-                                                 load(p, a, t)))))))
+                                         load(p, a, t) == And(Not(move(a, t)), at(p, loc(a, t), t), on(p, a, t+1))))))
 
     # Rule of unload: Airplane must not move and hold a package. Package must unload to airplane's location at t+1.
     s.add(ForAll([p], ForAll([a], ForAll([t],
-                                         And(
-                                             Implies(
-                                                 unload(p, a, t),
-                                                 And(Not(move(a, t)),
-                                                     on(p, a, t),
-                                                     at(p, loc(a, t), t+1))),
-                                             Implies(
-                                                 And(Not(move(a, t)),
-                                                     on(p, a, t),
-                                                     at(p, loc(a, t), t+1)),
-                                                 unload(p, a, t)))))))
+                                         unload(p, a, t) == And(Not(move(a, t)), on(p, a, t), at(p, loc(a, t), t+1))))))
 
     # Rule of Movement: Any plane at any time must either move or load/unload any number of packages (but not both)
     s.add(ForAll([a], ForAll([t], Xor(move(a, t), Exists([p], Or(load(p, a, t), unload(p, a, t)))))))
-
-
-"""
-    # All packages start at their source cities
-    s.add(a1 != a2)
-    s.add(p1 != p2 != p3)
-    s.add(c1 != c2 != c3 != c4)
-    s.add(at(p1, c1, 0))
-    s.add(at(p2, c4, 0))
-    s.add(at(p3, c3, 0))
-
-    # There exists a time where all packages end at their destination cities and it is minimal
-    s.add(at(p1, c3, t))
-    s.add(at(p2, c2, t))
-    s.add(at(p3, c1, t))
-    s.add(Implies(Exists[t0], And(at(p1, c3, t0), at(p2, c2, t0), at(p3, c1, t0)), t0 >= t))  # assert FV[f] == t
 
     res = s.check()
     if res == sat:
@@ -210,7 +191,7 @@ def get_transport_plan(nc, np, na, src, dst, start):
     city_airplanes = []
     airplane_packages = []
     return city_packages, city_airplanes, airplane_packages
-"""
+
 
 if __name__ == '__main__':
     print_problem(**example_problem)
