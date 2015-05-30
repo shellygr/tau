40a68eee383d0b9bf6a71dfbd32461bc4570ea3c
itay_pk <itaypk@gmail.com>
2015-05-30 19:49:24 +0300

    Simple example works

--- asv15_ex2/mbg.py
+++ asv15_ex2/mbg.py
@@ -1,9 +1,10 @@
 bottom = "bottom"
 top = "top"
-iota = 0
+iota = set()
 
 
 def join(a, b):
+    print "DBG: Join: " + str(a) + ", " + str(b)
     if a == bottom:
         return b
     elif b == bottom:
@@ -25,12 +26,23 @@ def meet(a, b):
         return a & b
 
 
-def assign_const(v, var_name, value):
-    print "assign_const: " + str(v)
-    v[var_name] = value
-    return v
+def remove_existing_value(vs, var_name):
+    return set(v for v in vs if v[0] != var_name)
 
 
-def assign_var(v, var_name, assigned_var_name):
-    v[var_name] = v[assigned_var_name]
-    return v
+def assign_const(vs, var_name, value):
+    print "DBG: assign_const: " + str(vs) + " := " + str(value)
+    # Note: for debugging purpose, it is possible to assign 'value' instead of 'top'
+    return remove_existing_value(vs, var_name) | set([(var_name, top)])
+
+
+def assign_var(vs, var_name, assigned_var_name):
+    # Do we know this variable?
+    for v in vs:
+        if v[0] == assigned_var_name:
+            print "DBG: Variable " + var_name + " found, its value is " + str(v[1])
+            # This variable exists - assign its value to our variable
+            return remove_existing_value(vs, var_name) | set([(var_name, v[1])])
+
+    # This is an unknown variable - this may be garbage
+    return remove_existing_value(vs, var_name) | set([(var_name, bottom)])
--- asv15_ex2/mbg_prog1.py
+++ asv15_ex2/mbg_prog1.py
@@ -8,7 +8,7 @@ from mbg import join, bottom, top, iota, assign_const, assign_var
 # Edges
 succ = {1: {2}, 2: {3}, 3: {}}
 # Transfer function
-tr = {(1, 2): lambda v: assign_const(v, 'a', 1), (2, 3): lambda v: assign_var(v, 'b', 'a'), (3, 3): lambda v: v}
+tr = {(1, 2): lambda v: assign_const(v, 'a', 5), (2, 3): lambda v: assign_var(v, 'b', 'c'), (3, 3): lambda v: v}
 # Textual description of the edges
 tr_txt = {(1, 2): "a := 5", (2, 3): "b := a", (3, 3): "nop"}
 
