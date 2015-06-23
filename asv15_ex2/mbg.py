3aa2d2c524f9e1728187a91c919f31423d30570b
itay_pk <itaypk@gmail.com>
2015-06-23 23:37:01 +0300

    Changed to work with a bag of variables

--- asv15_ex2/mbg.py
+++ asv15_ex2/mbg.py
@@ -1,10 +1,15 @@
 bottom = "bottom"
 top = "top"
-iota = set()
+is_debug = False
+
+
+def dbg_print(msg):
+    if is_debug:
+        print "DBG: " + msg
 
 
 def join(a, b):
-    print "DBG: Join: " + str(a) + ", " + str(b)
+    dbg_print("Join: " + str(a) + ", " + str(b))
     if a == bottom:
         return b
     elif b == bottom:
@@ -13,7 +18,6 @@ def join(a, b):
         return a
     else:
         return a | b
-
 
 def meet(a, b):
     if a == top:
@@ -31,18 +35,19 @@ def remove_existing_value(vs, var_name):
 
 
 def assign_const(vs, var_name, value):
-    print "DBG: assign_const: " + str(vs) + " := " + str(value)
+    dbg_print("assign_const: " + str(var_name) + " := " + str(value))
     # Note: for debugging purpose, it is possible to assign 'value' instead of 'top'
-    return remove_existing_value(vs, var_name) | set([(var_name, top)])
+    return remove_existing_value(vs, var_name)
 
 
 def assign_var(vs, var_name, assigned_var_name):
+    dbg_print(str(var_name) + " := " + str(assigned_var_name))
     # Do we know this variable?
     for v in vs:
         if v[0] == assigned_var_name:
-            print "DBG: Variable " + var_name + " found, its value is " + str(v[1])
-            # This variable exists - assign its value to our variable
-            return remove_existing_value(vs, var_name) | set([(var_name, v[1])])
+            dbg_print("Variable " + assigned_var_name + " found")
+            # This variable is assigned with a may-be-garbage variable - this may be garbage
+            return vs | set([var_name])
 
-    # This is an unknown variable - this may be garbage
-    return remove_existing_value(vs, var_name) | set([(var_name, bottom)])
+    # The assigned variable is not garbage - remove it from the list
+    return remove_existing_value(vs, var_name)
--- asv15_ex2/mbg_prog1.py
+++ asv15_ex2/mbg_prog1.py
@@ -1,16 +1,26 @@
 from chaotic import chaotic
-from mbg import join, bottom, top, iota, assign_const, assign_var
+from mbg import join, bottom, top, assign_const, assign_var
 
-# int a
+# int a, b, c
 # a = 5
-# b = 6
+# b = c
 
 # Edges
-succ = {1: {2}, 2: {3}, 3: {}}
+succ = {1: {2}, 2: {3}, 3: {4}, 4: {5}, 5: {}}
 # Transfer function
-tr = {(1, 2): lambda v: assign_const(v, 'a', 5), (2, 3): lambda v: assign_var(v, 'b', 'c'), (3, 3): lambda v: v}
+tr = {
+    (1, 2): lambda v: assign_const(v, 'a', 5),
+    (2, 3): lambda v: assign_var(v, 'b', 'c'),
+    (3, 4): lambda v: assign_var(v, 'c', 'b'),
+    (4, 5): lambda v: assign_var(v, 'a', 'c'),
+    (5, 5): lambda v: v}
 # Textual description of the edges
-tr_txt = {(1, 2): "a := 5", (2, 3): "b := a", (3, 3): "nop"}
+tr_txt = {
+    (1, 2): "a := 5",
+    (2, 3): "b := c",
+    (3, 4): "c := b",
+    (4, 5): "a := c",
+    (5, 5): "nop"}
 
-chaotic(succ, 1, iota, join, bottom, tr, tr_txt)
+chaotic(succ, 1, set([ 'a', 'b', 'c' ]), join, bottom, tr, tr_txt)
 
